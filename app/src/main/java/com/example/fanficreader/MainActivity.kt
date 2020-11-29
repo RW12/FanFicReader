489c96e1580a6e72061ca427549675839688de6d
Kevin Huang <kevlan229@gmail.com>
2020-11-28 20:18:58 -0600

    added button and launching fragment (#1)

--- app/src/main/java/com/example/fanficreader/MainActivity.kt
+++ app/src/main/java/com/example/fanficreader/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.fanficreader
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setButton()
+    }
+
+    fun setButton() {
+        val button = findViewById<Button>(R.id.activityMainButton)
+        button.setOnClickListener{
+            supportFragmentManager.beginTransaction().replace(R.id.activityMainContainer,StoryViewerFragment()).commit()
+        }
     }
 }
