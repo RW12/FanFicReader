db0e1d56928c0c0740181af92a7724d809d13200
kevinhuang96 <kevlan229@gmail.com>
2020-12-12 20:01:28 -0600

    created home screen fragment, added search bar, and toolbar; made it so main activity inflates home screen fragment

--- app/src/main/java/com/example/fanficreader/MainActivity.kt
+++ app/src/main/java/com/example/fanficreader/MainActivity.kt
@@ -2,19 +2,32 @@ package com.example.fanficreader
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setSupportActionBar(findViewById(R.id.mainToolbar))
         setContentView(R.layout.activity_main)
-        setButton()
+        if(savedInstanceState == null)
+            createHomeScreen()
+    }
+    //need onStart, onResume, onPause, onStop, onDestroy?
+    fun createHomeScreen() {
+        supportFragmentManager.beginTransaction().add(R.id.activityMainContainer,HomeScreen()).commit()
     }
 
-    fun setButton() {
-        val button = findViewById<Button>(R.id.activityMainButton)
-        button.setOnClickListener{
-            supportFragmentManager.beginTransaction().replace(R.id.activityMainContainer,StoryViewerFragment()).commit()
-        }
+    /**
+     * onClick listener for the 'Profile' button; empty for now
+     * eventually replace the text on the button with an image (in the xml file)?
+     */
+    fun setProfileButton() {
+
+    }
+    /**
+     * onClick listener for the 'Settings' button; empty for now
+     * eventually replace the text on the button with an image (in the xml file)?
+     */
+    fun setSettingsButton() {
+
     }
 }
